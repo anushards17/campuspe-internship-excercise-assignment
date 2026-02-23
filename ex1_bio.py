6b63ba0fe4daa16ac9089200bba1cb0a5a2d2977
anushards17 <anushards17@gmail.com>
2026-02-23 15:59:41 +0530

    Add files via upload

---
+++ ex1_bio.py
@@ -0,0 +1,14 @@
+name = "Anu Sharadha"
+age = 20
+course = "Gen AI"
+college = "Don Bosco University"
+email = "anushards17@gmail.com"
+print("+--------------------------------------+")
+print("| STUDENT BIO CARD                     |")
+print("+--------------------------------------+")
+print("| Name    :", name)
+print("| Age     :", age, "years")
+print("| Course  :", course)
+print("| College :", college)
+print("| Email   :", email)
+print("+--------------------------------------+")
