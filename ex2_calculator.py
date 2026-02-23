a0e593a0ddc3b6c733cbf96e09647a40d0d03ab0
anushards17 <anushards17@gmail.com>
2026-02-23 20:45:51 +0530

    Update ex2_calculator.py
    Added question number, file name, program title, and detailed description at the top of the file. Improved output formatting by labeling each arithmetic operation clearly and handled division by zero properly.

--- ex2_calculator.py
+++ ex2_calculator.py
@@ -1,3 +1,12 @@
+# -------------------------------------------------------
+# Question No: 2
+# File Name : ex2_calculator.py
+# Program   : Simple Calculator
+# Description:
+# This program takes two numbers as input and performs
+# basic arithmetic operations like addition, subtraction,
+# multiplication, division, modulus, and exponentiation.
+# -------------------------------------------------------
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number:"))
 print("Results:")
@@ -9,4 +18,5 @@ if num2 != 0:
 else: 
     print("can't divide by zero")
 print(num1 % num2)
+
 print(num1 ** num2)
