1fa297be829353cf19dd1b9a642667f5e92c8dca
anushards17 <anushards17@gmail.com>
2026-02-23 20:41:54 +0530

    Update ex1_bio.py
    Included question number, file name, program title, and description at the top of the file to meet assignment requirements.

--- ex1_bio.py
+++ ex1_bio.py
@@ -1,3 +1,9 @@
+# -------------------------------------------------------
+# Question No: 1
+# File Name : ex1_bio.py
+# Program   : Student Bio Card
+# Description:This program displays a formatted student bio card using basic variables and print statements.
+# -------------------------------------------------------
 name = "Anu Sharadha"
 age = 20
 course = "Gen AI"
@@ -11,4 +17,5 @@ print("| Age     :", age, "years")
 print("| Course  :", course)
 print("| College :", college)
 print("| Email   :", email)
+
 print("+--------------------------------------+")
